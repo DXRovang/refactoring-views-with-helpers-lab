0144b973626ce114325cf433cabb8ba9c48f61d3
Alexander Rovang <wolf_vii@yahoo.com>
2021-03-19 12:23:06 -0400

    commit tree 19f187127c59bdb054d1e2962b03dabe7e9c2b1d

--- app/helpers/songs_helper.rb
+++ app/helpers/songs_helper.rb
@@ -1,10 +1,10 @@
 module SongsHelper
-  def display_artist
+  def display_artist(song)
     # binding.pry
-    if @song.artist_name != ""
-      link_to @song.artist_name, artist_path(@song.artist.id)
+    if song.artist_name != nil
+      link_to song.artist_name, artist_path(song.artist.id)
     else
-      link_to "Add Artist", edit_song_path(@song)
+      link_to "Add Artist", edit_song_path(song)
     end
   end
 end
